4ebcff7eb86c66fad48ceb6d01422ea70be65645
Varatha1 <varathuibo1@gmail.com>
2026-01-08 16:18:30 +0530

    Add files via upload

---
+++ Revenue by Category.sql
@@ -0,0 +1,7 @@
+
+use projec1;
+
+select p.category, sum(oi.quantity*oi.item_price) as revenue
+from orders_items oi
+join product p on oi.product_id = p.product_id
+group by p.category;
---
+++ Top 10 Customers by Revenue.sql
@@ -0,0 +1,8 @@
+use projec1;
+
+select c.customer_id, c.name_s, sum(o.total_amount) as revenue
+from customer c
+join orders O ON c.customer_id = o.customer_id
+group by c.customer_id, c.name_s
+order by revenue desc 
+limit 10;
